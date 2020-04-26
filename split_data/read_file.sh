8aaf3637fb585a098aa4c668d63327e293f1dbec
cesar <roaldes.cesar@gmail.com>
2020-04-26 12:23:56 +0200

    notebook + segmentation data

---
+++ split_data/read_file.sh
@@ -0,0 +1,4 @@
+for i in /home/cesar/cours/ensae/donnees_distrib/projet/mobile_data/split_data/split/*/*; do 
+	echo $i
+	(cat $i | split -l 100000 --filter="~/.local/kafka_2.12-2.2.0/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic antennes; sleep 1" > /dev/null ) &
+done
